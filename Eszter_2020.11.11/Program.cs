7693fe58ee060e42a92c03eb959e7db34c251cee
varzoltan <64203391+varzoltan@users.noreply.github.com>
2020-11-17 15:03:28 +0100

    15:03

--- Eszter_2020.11.11/Program.cs
+++ Eszter_2020.11.11/Program.cs
@@ -12,8 +12,19 @@ namespace Eszter_2020._11._11
         {
             //1.feladat
             Console.Write("Kérem adjon meg egy karaktert az angol ABC szerint: ");
-            string karakter = Console.ReadLine();
+            string karakter = Console.ReadLine().ToUpper();
+            //string szam = CharToSMS(karakter);
+            Console.WriteLine($"1. feladat: a beolvasott karakter száma: {CharToSMS(karakter)}");
 
+            //2.feladat
+            Console.Write("Kérem adjon meg egy szót: ");
+            string szo = Console.ReadLine().ToUpper();
+            string osszefuz = null;
+            for (int i = 0;i<szo.Length;i++)
+            {
+                osszefuz = CharToSMS(szo[i].ToString());
+            }
+            Console.ReadKey();
         }
 
         //Függvény: 1 karaktert alakít át sms számmá
@@ -23,6 +34,36 @@ namespace Eszter_2020._11._11
             {
                 return "2";
             }
+            else if (kar == "D" || kar == "E" || kar == "F")
+            {
+                return "3";
+            }
+            else if (kar == "G" || kar == "H" || kar == "I")
+            {
+                return "4";
+            }
+            else if (kar == "J" || kar == "K" || kar == "L")
+            {
+                return "5";
+            }
+            else if (kar == "M" || kar == "N" || kar == "O")
+            {
+                return "6";
+            }
+            else if (kar == "P" || kar == "Q" || kar == "R" || kar == "S")
+            {
+                return "7";
+            }
+            else if (kar == "T" || kar == "U" || kar == "V")
+            {
+                return "8";
+            }
+            else
+            {
+                return "9";
+            }
         }
+
+
     }
 }
